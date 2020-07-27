df44710a404355629a211cccb332153665b3a928
duhong-hub <147443966@qq.com>
2020-07-27 18:03:53 +0800

    视频教学--添加，编辑，删除弹框，及交互逻辑

--- views/set/course.js
+++ views/set/course.js
@@ -2,8 +2,9 @@ layui.config({
     base: '../../layuiadmin/' //静态资源所在路径
 }).extend({
     index: 'lib/index' //主入口模块
-}).use(['index', 'table', 'jquery','form','laydate'], function() {
+}).use(['index', 'table', 'jquery','form','laydate','upload'], function() {
     var table = layui.table,
+        upload = layui.upload,
         setter = layui.setter,
         laydate = layui.laydate,
         form = layui.form;
@@ -418,116 +419,40 @@ layui.config({
     })
     var $ = layui.$,
         active = {
-            //点击搜索
-            search: function() {
-                table.render({
-                    elem: '#test-table-operate',
-                    height: 'full-100',
-                    url: "https://f.longjuli.com/meeting/findMeetingBylayui" //数据接口
-                        ,
-                    xhrFields: {
-                        withCredentials: true
-                    },
-                    where: {
-                        "rule": $('#demoReload').val(),
-                        "status":0
-                    },
-                    method: 'get',
-                    page: {
-                        layout: ['prev', 'page', 'next', 'count', 'skip']
-                    },
-                    cols: [
-                        [ //表头
-                            {
-                                type: 'checkbox',
-                                fixed: 'left',
-                            },
-                            {
-                                field: 'id',
-                                title: '序号',
-                                unresize: 'false',
-                                width:60,
-                            },
-                            {
-                                width: 100,
-                                title: '操作',
-                                toolbar: '#test-table-operate-barDemo',
-                            },
-                            {
-                                field: 'name',
-                                title: '姓名',
-                                align: 'left',
-                            }, {
-                                field: 'roomname',
-                                title: '手机号',
-                                align: 'left',
-                            },
-                            {
-                                field: 'roomname',
-                                title: '角色',
-                                align: 'left',
-                                templet: function(data) {
-                                        return data.name + "<span class='layui-badge table-icon-style2'>2</span>"
-                                },
-                            },
-                            {
-                                field: 'roomname',
-                                title: '备注',
-                                align: 'left',
-                            },
-                            {
-                                field: 'roomname',
-                                title: '最近登录',
-                                align: 'left',
-                                templet: function(data) {
-                                    return data.name + "<i class='layui-icon table-icon-style3'>&#xe60e;</i>"
-                                },
-                            },
-                            {
-                                field: 'modifytime',
-                                title: '创建时间',
-                                align: 'left',
-                            },
-                            
-                        ]
-                    ],
-                    event: true,
-                    page: true,
-                    limit: 15,
-                    skin: 'line',
-                    even: true,
-                    limits: [5, 10, 15],
-                    done: function(res, curr, count) {
-                        table_data = res.data;
-
-                        layer.closeAll('loading');
-                        arrangeList.length = 0;
-                        // layer.close(layer.index); //它获取的始终是最新弹出的某个层，值是由layer内部动态递增计算的
-                        // layer.close(index);    //返回数据关闭loading
-                    },
-                });
-            },
             //点击添加
-            add: function() {
+            addcourse: function() {
                 layer.open({
                     type: 2,
-                    title: '新增账号',
-                    area: ['500px', '400px'],
-                    btn: ['保存', '取消'],
+                    title: '添加教程',
+                    area: ['500px', '300px'],
+                    btn: ['确定', '取消'],
                     btnAlign: 'c',
                     maxmin: true,
-                    content: 'account_add_pop.html',
+                    content: 'course_add_pop.html',
                     yes: function(index, layero) {
                         var submit = layero.find('iframe').contents().find("#ruleclick");
                         submit.click();
                     }
                 });
             },
-            //点击删除
-            del: function() { 
-                if ( arrangeList.length == 0 ) {
-                    return layer.msg("请选择再批量删除")
-                }
+            //点击编辑
+            editcourse: function() {
+                layer.open({
+                    type: 2,
+                    title: '编辑教程',
+                    area: ['500px', '300px'],
+                    btn: ['确定', '取消'],
+                    btnAlign: 'c',
+                    maxmin: true,
+                    content: 'course_add_pop.html',
+                    yes: function(index, layero) {
+                        var submit = layero.find('iframe').contents().find("#ruleclick");
+                        submit.click();
+                    }
+                });
+            },
+             //点击删除
+             delcourse: function() { 
                 layer.confirm('&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;删除后无法恢复！确定删除吗？&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;',{title:'删除提醒',btnAlign:'c'}, function() {//获取选中数目
                 $.ajax({
                         async: false,
@@ -557,96 +482,23 @@ layui.config({
                     })
                 })
             },
-            //重置密码
-            resetPassword: function() { 
-                if ( arrangeList.length == 0 ) {
-                    return layer.msg("请选择再批量重置密码")
-                }
-                layer.confirm('&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;将自动发送随机密码到指定的手机上？&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;',{title:'重置密码',btnAlign:'c'}, function() {//获取选中数目
-                $.ajax({
-                        async: false,
-                        type: "post",
-                        // url: url+"/roomtemplate/batchRemove",
-                        dataType: "json",
-                        //成功的回调函数
-                        data: {
-                            "roomid":arrangeList.join(",")
-                        },
-                        xhrFields: {
-                            withCredentials: true
-                        },
-                        success: function(msg) {
-                            if (msg.code == 0) {
-                                layer.msg("密码发送成功");
-                                reloaddata(); // 父页面刷新
-                            } else {
-                                layer.msg(msg.msg);
-                            }
-                
-                        },
-                        //失败的回调函数
-                        error: function() {
-                            console.log("error")
-                        }
-                    })
-                })
-            },
-            //刷新
-            refresh: function() {
-                reloads();
-                // reloaddata(); // 父页面刷新
-            },
         };
     //给页面里的layui-dS 绑定事件
-    $('.layui-ds').on('click', function() {
+    $('.layui-btn').on('click', function() {
         var type = $(this).data('type');
         active[type] ? active[type].call(this) : '';
     });
 
-    /*右侧菜单HOVER显示提示文字*/
-    var subtips;
-    $('.pop_text button').each(function(){
-        var _id = $(this).attr('id');
-        var _data = $(this).attr('data');
-        $("#" + _id).hover(function() {
-            openMsg();
-        }, function() {
-            if(subtips){
-                layer.close(subtips);
-            }
-        });
-        function openMsg() {
-            subtips = layer.tips(_data, '#'+_id,{tips:[3,'#666'],time: 30000});
-        }
-    })
-    /*右侧菜单HOVER显示提示文字 end*/
-
-    /* 点击查看更多操作 三部分组成 CSS html js 3.10 */
-    // if($(".layui-table-header table tr").length==1){
-    //     $(".layui-table-header").css("min-height",195)
-    // }else{
-    //     $("table").find("tr:last").find("td:nth-child(6)").find(".moreOperateA").css("top",-55);
-    //     $("table").find("tr:last").find("td:nth-child(6)").find(".moreOperateArr").css({"top":49,"background-image":"url('../../../images/tips_darr.png')"});
-    // }
-    $('.moreOperate').each(function(){
-        $(this).hover(function() {
-            var X = $(this).offset().left;
-            var parentX = $(this).parent().offset().left;
-
-            console.log(X)
-            console.log(parentX)
-            $(this).children(".moreOperateA").css("left",X-parentX-37).show();
-        }, function() {
-            $(this).children(".moreOperateA").hide();
-        });
-    })
-
-
-
-
-    /* 点击查看更多操作 三部分组成 CSS html js end 3.10 */
-
-
+//重新上传视频
+upload.render({
+    elem: '#uploadcourse'
+    ,url: 'https://httpbin.org/post' //改成您自己的上传接口
+    ,accept: 'video' //视频
+    ,done: function(res){
+      layer.msg('上传成功');
+      console.log(res)
+    }
+});
 
 
 });
---
+++ views/set/course_add_pop.js
@@ -0,0 +1,130 @@
+layui.config({
+    base: '../../layuiadmin/' //静态资源所在路径
+}).extend({
+    index: 'lib/index' //主入口模块
+}).use(['index', 'form','jquery','tree','util','upload'], function () {
+    var $ = layui.$,
+    upload = layui.upload,
+    setter = layui.setter,
+    form = layui.form,
+    layer = layui.layer,
+    setter = layui.setter,
+    router = layui.router(),
+    data = '';
+    // var server = setter.baseUrl;
+
+    var server = setter.baseUrl;
+    var uri = window.location.search;
+    
+    var role_ID = setter.getUrlParam("role_ID",uri) || "";
+
+
+    //视频
+    upload.render({
+        elem: '#test5'
+        ,url: 'https://httpbin.org/post' //改成您自己的上传接口
+        ,accept: 'video' //视频
+        ,done: function(res){
+          layer.msg('上传成功');
+          console.log(res)
+        }
+      });
+
+    if(role_ID){
+        //编辑
+        $.Ajax({
+            async: false,
+            url: server + "/ADMINM/role/toEdit",
+            dataType: "json",
+            method: 'get',
+            data:{"ROLE_ID":role_ID},
+            success: function(obj) {
+                if(obj.code == 1){
+                    changeRoleHtml(obj.data || {});
+                }else{
+                    layer.msg(obj.msg || "获取角色详情错误");
+                }
+               
+            }
+        });
+    }
+
+    //监听提交
+    form.on('submit(submit)', function(data){
+        // alert(888)
+        // layer.msg(JSON.stringify(data.field),function(){
+        //     location.href='index.html'
+        // });
+        console.log(data.field)
+        var condi = {};
+        condi = data.field;
+        //写死组id
+        condi.PARENT_ID = 1;
+
+        if(role_ID){
+            //编辑
+            editRole(condi);
+        }else{
+            addRole(condi);
+        }
+        
+        return false;
+    });
+
+    function changeRoleHtml(obj){
+        $("#ROLE_NAME").val(obj.ROLE_NAME || "");
+        $("#BZ").val(obj.BZ || "");
+    }
+
+    function addRole(condi){
+        $.Ajax({
+            async: false,
+            url: server + "/ADMINM/role/add",
+            dataType: "json",
+            method: 'post',
+            data:condi,
+            success: function(obj) {
+                if(obj.code == 1){
+                    layer.msg("添加成功");
+
+                    setTimeout(function(){
+                        //刷新父页面
+                        window.parent.location.reload();
+                        var index = parent.layer.getFrameIndex(window.name);
+               		    parent.layer.close(index);
+                    },1500);
+                }else{
+                    layer.msg(obj.msg || "添加失败");
+                }
+            }
+        });
+    }
+
+    function editRole(condi){
+        condi.ROLE_ID = role_ID;
+
+        $.Ajax({
+            async: false,
+            url: server + "/ADMINM/role/edit",
+            dataType: "json",
+            method: 'post',
+            data:condi,
+            success: function(obj) {
+                if(obj.code == 1){
+                    layer.msg("修改成功");
+
+                    setTimeout(function(){
+                        //刷新父页面
+                        window.parent.location.reload();
+                        var index = parent.layer.getFrameIndex(window.name);
+               		    parent.layer.close(index);
+                    },1500);
+                }else{
+                    layer.msg(obj.msg || "修改失败");
+                }
+            }
+        });
+    }
+    
+
+});
